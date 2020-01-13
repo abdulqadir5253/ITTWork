325348cf93a0310c54f2fcc03d8062ab593056b5
Abdul Qadir <abdul5152@ymail.com>
2020-01-13 11:04:21 +0530

    Update InfoPage.java

--- InfoPage.java
+++ InfoPage.java
@@ -1,52 +1,9 @@
 package week1Assignmt;
 
-interface impDetail{
-	String getAddrs();
-	String getAcctDet();
-	String getContactNo();
-	void setAddrs(String a);
-	void setAcctDet(String a);
-	void setContactNo(String a);
-}
-
-class GetDetails implements impDetail{
-	private String city;
-	private String accNo;
-	private String contact;	
-	
-	public String getAddrs(){
-		return city;
-	}
-	
-	public String getAcctDet(){
-		return accNo;
-	}
-	
-	public String getContactNo(){
-		return contact;
-	}
-	
-	public void setAddrs(String city){
-		this.city = city;
-	}
-	
-	public void setAcctDet(String accNo){
-		this.accNo = accNo;
-	}
-	
-	public void setContactNo(String contact){
-		this.contact = contact;
-	}
-}
-
 public class InfoPage {
 	SetInfo abdul = new SetInfo();
 	SetInfo ansh = new SetInfo();
-	SetInfo manoj = new SetInfo();
-	GetDetails abdul1 = new GetDetails();
-	GetDetails ansh1 = new GetDetails();
-	GetDetails manoj1 = new GetDetails();
-	
+	SetInfo manoj = new SetInfo();	
 			
 	public String infoMethod(String username){
 		abdul.setDseg("Executive");
@@ -58,21 +15,15 @@ public class InfoPage {
 		else if(username.equals("manoj sharma"))
 			return manoj.getDseg();
 		else 
-			return ansh.getDseg();
-				
-	}	
-	
-	public void getGenInfo(String username){
-		
-	}
+			return ansh.getDseg();			
+	}		
 }
 
 class SetInfo{
 	private String dseg;
 	
 	public void setDseg(String dseg){
-		this.dseg = dseg;
-		
+		this.dseg = dseg;		
 	}
 	
 	public String getDseg(){
