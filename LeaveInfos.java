b495fc7b0aa02e5874a30f0d903979389830f644
Abdul Qadir <abdul5152@ymail.com>
2020-01-16 09:57:26 +0530

    Update LeaveInfos.java

--- LeaveInfos.java
+++ LeaveInfos.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 
 class Employee{
 	private int leavesAvl;
-	private int leavesTaken;
-//	private static int leaveApplication;
-	private boolean leaveStatus;	
+	private int leavesTaken;	
+	private int leaveStatus;
+	
+	// -1 Leave Cancelled
+	// 0 No application
+	// 1 Leave Granted
+	// 2 Pending for approval
 	
 	
 	Employee(){
 		leavesAvl = 10;
 		leavesTaken = 0;
-		leaveStatus = false;		
+		leaveStatus = 0;		
 	}
 	
 	int getLeavesAvl(){
@@ -25,11 +29,15 @@ class Employee{
 	void setLeavesAvl(int leave){
 		leavesAvl = leave;
 	}
-	void setLeaveStatus(boolean status){
-		leaveStatus = status;
+	void setLeaveStatus(int status){
+		this.leaveStatus = status;
 	}
 	
-	boolean getLeaveStatus(){
+	void setLeavesTaken(int taken){
+		this.leavesTaken = taken;
+	}
+	
+	int getLeaveStatus(){
 		return leaveStatus;
 	}
 }
@@ -39,6 +47,19 @@ class LeaveManage{
 	Employee ansh = new Employee();
 	static Scanner scan = new Scanner(System.in);
 	
+	
+	void setApplicationStatus(String username){
+		if(username.equals("abdul")){
+			abdul.setLeaveStatus(2);
+			leaveInfo(abdul);
+		}
+			
+		else if(username.equals("ansh")){
+			ansh.setLeaveStatus(2);
+			leaveInfo(ansh);
+		}
+			
+	}
 	
 	public static void leaveInfo(Employee emp){
 		System.out.println("Leaves Available: "+emp.getLeavesAvl());
@@ -53,12 +74,14 @@ class LeaveManage{
 			System.out.println("Leaves Approved");
 			int leavesAvl =  emp.getLeavesAvl() - noOfDays;
 			emp.setLeavesAvl(leavesAvl);
-			abdul.setLeaveStatus(true);
+			int leavesTaken = emp.getLeavesTaken() + noOfDays;
+			emp.setLeaveStatus(1);
+			emp.setLeavesTaken(leavesTaken);
 //			System.out.println("status: "+emp.getLeaveStatus());
 		}
 		else if(dec == 2){
 			System.out.println("Leave Application Declined");
-			emp.setLeaveStatus(false);
+			emp.setLeaveStatus(-1);
 		}
 		
 		System.out.println("1. for Logout");
@@ -67,7 +90,7 @@ class LeaveManage{
 			new Index().login();			
 	}
 	
-	public boolean checkStatus(String username){
+	public int checkStatus(String username){
 		
 		if(username.equals("abdul qadir")){
 //			System.out.println("abdul.getLeaveStatus() "+ abdul.getLeaveStatus());
@@ -76,7 +99,7 @@ class LeaveManage{
 			
 		else if(username.equals("ansh arora"))
 			return ansh.getLeaveStatus();
-		return false;
+		return -1;
 	}
 	
 	public void manageLeave(String username, int noOfDays){
@@ -93,45 +116,6 @@ class LeaveManage{
 	
 	
 }
-
-//class ExecutiveEmp{
-//	Scanner scan = new Scanner(System.in);
-//	static int leavesAvl = 10;
-//	static int leavesTaken = 0;
-//	static int leaveApplication = 0;
-//	static boolean leaveStatus = false;
-//	static String reason ;
-//	void grant(int leaves){
-//		leaveApplication = 0;
-//		System.out.println("Reason for Leave: "+reason);
-//		System.out.println("Number Of Days of Leave: "+leaves);
-//		System.out.println("Leaves Available for Employee: "+leavesAvl);
-//		System.out.println("Enter option:\n1.Approve Leaves\n2.Do not Approve");
-//		int decsion = scan.nextInt();
-//		if(decsion == 1){			
-//			System.out.println("Leaves Approved");
-//			leaveStatus = true;
-//			System.out.println("Press 1 for Logout");
-//			int co = scan.nextInt();
-//			if(co == 1)
-//				new Index().login();			
-//		}
-//		else{
-//			System.out.println("Leaves Not Approved");
-//			leaveStatus = false;
-//			System.out.println("Press 1 for Logout");			
-//			int co = scan.nextInt();
-//			if(co == 1)
-//				new Index().login();
-//			
-//		}			
-//	}
-//	
-//	int checkLeaves(){
-//		return leavesAvl;
-//	}
-//	
-//}
 
 class RequestLeave{
 	private int request ;
@@ -176,51 +160,47 @@ public class LeaveInfos {
 	// Called by Manager
 	LeaveInfos(){
 		System.out.println("Request: "+ abdul.getRequest());
-		System.out.println("Reason "+abdul.getReason());
+//		System.out.println("Reason "+abdul.getReason());
 		if(abdul.getRequest()==1){
+			System.out.println("Name Of Employee: Abdul");
 			System.out.println("Reason for Leave Application: "+abdul.getReason());
-			System.out.println("Number of Days Of Leave: "+ abdul.getNoOfDays());
+			System.out.println("Number of Days Of Leave: "+ abdul.getNoOfDays());					
+			
 			new LeaveManage().manageLeave("abdul", abdul.getNoOfDays());
 		}
 			
 		if(ansh.getRequest() == 1){
+			System.out.println("Name of Employee: Ansh");
 			System.out.println("Reason for Leave Application: "+ansh.getReason());
-			System.out.println("Number of Days Of Leave: "+ ansh.getNoOfDays());
+			System.out.println("Number of Days Of Leave: "+ ansh.getNoOfDays());					
+			
 			new LeaveManage().manageLeave("ansh", ansh.getNoOfDays());
 		}
-		
-//		ExecutiveEmp emp = new ExecutiveEmp();
-//		emp.grant(noOfDays);			
-		
+				
 	}
 	
-	// Called by Executive And Lead
+	// Called by Executive And Lead to apply for leaves
 	LeaveInfos(String choice, int noOfDays, String username){
 		
 		leaveApplication =1;
 		if(username.equals("abdul qadir")){
 						
-			System.out.println("Request set 1");
+			// Set Application Status to 2(Pending)
+			new LeaveManage().setApplicationStatus("abdul");
 			abdul.setRequest(1);
 			abdul.setReason(choice);
 			abdul.setNoOfDays(noOfDays);
+			System.out.println();
 		}
 		
 		else if(username.equals("ansh arora")){
+			// Set Application Status to 2(Pending)			
+			new LeaveManage().setApplicationStatus("ansh");
 			ansh.setRequest(1);
 			ansh.setReason(choice);
 			ansh.setNoOfDays(noOfDays);
 		}
 			
-		
-		
-//		LeaveManage manang = new LeaveManage();
-//		manang.manageLeave(choice, noOfDays, username);
-//		this.noOfDays = noOfDays;
-//		
-//		ExecutiveEmp emp = new ExecutiveEmp();
-//		emp.leaveApplication = 1;
-//		emp.reason = choice;
 		System.out.println("Application Sent!");
 		System.out.println("1. for Logout");
 		int logout = scan.nextInt();
